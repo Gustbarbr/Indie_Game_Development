800665b272be8d83c5f4e134a25c3182c7f6f548
Gustavo Gomes Barbosa <gustbarbr@gmail.com>
2025-06-11 22:20:55 -0300

    Adding exhaustion, sprinting and camera follow

--- FLORUMBRA/Assets/Scripts/Player/PlayerControl.cs
+++ FLORUMBRA/Assets/Scripts/Player/PlayerControl.cs
@@ -12,6 +12,7 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] float velocity;
     [SerializeField] float baseSpeed;
     [SerializeField] float sprintVelocity;
+    [SerializeField] bool exhaustion = false;
 
     public Slider hpBar;
     public Slider manaBar;
@@ -30,6 +31,39 @@ public class PlayerControl : MonoBehaviour
     public void Movement()
     {
         float HorizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        bool sprint = Input.GetKey(KeyCode.LeftShift);
+        float stamina = staminaBar.value;
+
+        // Se o player apertar para correr e não estiver exausto, vai começar a correr
+        if (sprint && !exhaustion)
+        {
+            velocity = baseSpeed + sprintVelocity;
+            stamina -= 0.2f * Time.deltaTime;
+            if (stamina < 0) stamina = 0;
+            // Se a stamina for 0 entra em exaustão
+            if (stamina == 0) exhaustion = true;
+        }
+
+        // Se exausto fica mais lento e não pode correr
+        else if (exhaustion)
+        {
+            velocity = baseSpeed - sprintVelocity;
+            stamina += 0.1f * Time.deltaTime;
+            // Se a stamina chegar a 30% pode voltar a correr
+            if (stamina >= 0.3f) exhaustion = false;
+        }
+
+        // Player anda com velocidade padrão e regenera stamina
+        else if (!sprint)
+        {
+            velocity = baseSpeed;
+            stamina += 0.2f * Time.deltaTime;
+            if (stamina > 1) stamina = 1;
+        }
+
+        // Atualiza a barra de stamina
+        staminaBar.value = stamina;
 
         rb.velocity = new Vector2(HorizontalMovement * velocity, rb.velocity.y);
     }
